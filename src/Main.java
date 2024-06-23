50aff0404a83c2bf8faf3b35dfe464e61e3d0589
Paul Franklin <paul.franklin@csuglobal.edu>
2024-06-23 18:28:45 -0400

    initial save

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+public class Main {
+    public static void main(String[] args) {
+        CustomLinkedList linkedList = new CustomLinkedList();
+
+        // Insert some elements
+        linkedList.insert(1);
+        linkedList.insert(2);
+        linkedList.insert(3);
+
+        // Iterate and display elements
+        Iterator<Integer> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
+}
+
